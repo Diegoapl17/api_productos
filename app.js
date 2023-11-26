7964c17c9cf643be157debfcb038865aa6c941ae
Diegoapl17 <perezlopezdiego037@gmail.com>
2023-11-26 13:19:40 -0500

    Actualización en app.js13

--- app.js
+++ app.js
@@ -6,26 +6,5 @@ const server = new Server()//instanciar el objeto
 
 server.app.use(express.json());
 
-// Ruta para recibir peticiones por nombre de usuario
-route.get('/:id', productoGetId); 
-
-const productoGetId =  async (req, res) => {
- 
-const { idProducto } = req.params; 
-
-try {
-    const usuarioID = await ProductoSchema.findById(idProducto);
-
-    if (!usuarioID) {
-        return res.status(404).json({ mensaje: 'Usuario no encontrado' });
-    }
-    res.json({ usuarioID });
-} catch (error) {
-    console.error('Error al buscar usuario por ID:', error);
-    res.status(500).json({ mensaje: 'Error interno del servidor' });
-}
-
-};
-
 server.listen()//escuchar las peticiones del servidor para saber si funciona Servidor local iniciarlo
 
